562fdfadedb8572d07ff426b9bbcf45110d57ff0
nexus4812 <powerbatteryboy@gmail.com>
2024-03-31 18:28:39 +0900

    create list endpoint

--- src/taskHandler.js
+++ src/taskHandler.js
@@ -1,6 +1,27 @@
 import { DynamoDB } from 'aws-sdk'
 import crypto from 'crypto'
 
+export async function list(event, context) {
+    const dynamodb = new DynamoDB({
+        region: 'ap-northeast-1'
+    })
+
+    const result = await dynamodb
+        .scan({
+            TableName: 'tasks'
+        })
+        .promise()
+
+    const tasks = result.Items.map((item) => {
+        return {
+            id: item.id.S,
+            title: item.title.S
+        }
+    })
+
+    return { tasks: tasks }
+}
+
 export async function post(event, context) {
     const requestBody = JSON.parse(event.body)
 
